354f37a5b96d869b26c8312401d6b9a2db7474a1
Sanchit <sanchit522@gmail.com>
2020-10-10 15:30:51 +0530

    Updates

---
+++ precache-manifest.6779f09c1ae5a1ad7c081ead2dc82cb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d80eb5d00531f88f53839e3df3eb25",
+    "url": "/involvio-shows/index.html"
+  },
+  {
+    "revision": "f67d4d79c2b293398f99",
+    "url": "/involvio-shows/static/css/main.0ba069c1.chunk.css"
+  },
+  {
+    "revision": "d5cf4af5961411764965",
+    "url": "/involvio-shows/static/js/2.0f8b614f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/involvio-shows/static/js/2.0f8b614f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67d4d79c2b293398f99",
+    "url": "/involvio-shows/static/js/main.1126e3fb.chunk.js"
+  },
+  {
+    "revision": "a6ba9c927206b128432d",
+    "url": "/involvio-shows/static/js/runtime-main.9b8c689e.js"
+  }
+]);
